57b6937ac3e08a4f0f060ae7eaba34545743db0c
Jairo Montes Toyos <ish71606@educastur.es>
2026-09-17 13:31:35 +0200

    Completados Ejercicio 1 y Ejercicio 2 de variables

---
+++ src/01-variables/exercises.js
@@ -0,0 +1,14 @@
+// EJER 1
+
+let nombre = "Jairo";
+console.log(nombre);
+nombre = "Jairo";
+console.log(nombre);    
+
+
+// EJER 2
+
+let constante = "PI";
+console.log(constante);
+constante = "3.1416";
+console.log(constante);
