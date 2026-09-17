b7b8c821eabf82d52a1ff596fce255d9a5071efa
Jairo Montes Toyos <ish71606@educastur.es>
2026-09-17 13:47:03 +0200

    Añadido ejercicio 3 correctamente

--- src/01-variables/exercises.js
+++ src/01-variables/exercises.js
@@ -12,3 +12,30 @@ let constante = "PI";
 console.log(constante);
 constante = "3.1416";
 console.log(constante);
+
+
+// EJER 3
+
+let edad;
+console.log(edad);
+edad = 26;
+console.log(edad);
+
+
+// EJER 4
+
+let precioBase = 100;
+let iva = 0.21;
+let precioFinal = precioBase + (precioBase * iva);
+console.log(precioFinal);
+
+
+// EJER 5
+
+let x = 1;
+{
+  let x = 2;
+  console.log(x);
+}
+console.log(x);
+
