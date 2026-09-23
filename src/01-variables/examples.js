99d3bca3abc9b358ac9d6cb59bb7e090499dbb6c
Jairo Montes Toyos <ish71606@educastur.es>
2026-09-23 20:07:38 +0200

    Añade ejemplos de variables

--- src/01-variables/examples.js
+++ src/01-variables/examples.js
@@ -2,5 +2,21 @@ function saludar(nombre) {
   return "Hola, " + nombre;
 }
 
-console.log(saludar("Ana"));
+console.log(saludar("Jairo"));
 
+
+let cliente = "Jairo";
+cliente = "Pepito";
+cliente = 20;
+console.log(cliente); // 20
+
+
+let precio;
+console.log(precio); // undefined
+precio = 20.5;
+console.log(precio); // 20.5
+
+
+const persona = { nombre: "Jairo", edad: 10 };
+persona.edad = 21; // válido: mutamos el objeto
+// persona = {};    // inválido: reasignación del enlace
