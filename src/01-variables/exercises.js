1a5bced591451e829a8739d6cca8dfdd06ba01e6
Jairo Montes Toyos <ish71606@educastur.es>
2026-09-23 20:25:15 +0200

    Estructura inicial de los apuntes de JavaScript

--- src/01-variables/exercises.js
+++ src/01-variables/exercises.js
@@ -28,3 +28,39 @@ let precioBase = 100;
 let iva = 0.21;
 let precioFinal = precioBase + (precioBase * iva);
 console.log(precioFinal);
+
+// EJER 5 
+let x = 1; 
+{ 
+    let x = 2; 
+    console.log(x); 
+} 
+console.log(x); 
+
+// EJER 6 
+
+const lista = [1, 2];
+lista.push(3);
+console.log(lista);
+lista = [];  // Pensé que daría error aquí por ser 'const'
+
+
+// Mi predicción falló porque confundí "modificar" con "reasignar". 
+// 'const' no hace que el array sea inmutable; permite añadir elementos con '.push()'.
+
+
+// EJER 7 
+
+let nombreUsuario = "Ana"; 
+let edadUsuario = 30; 
+let esActiva = true; 
+let salarioMensual = 1250.5; 
+
+// RETO 
+
+// Con var: la variable tiene alcance global/función, imprime 3, 3, 3
+for (var i = 0; i < 3; i++) { 
+    setTimeout(() => console.log(i), 0); 
+} 
+
+// Con let: la variable tiene alcance de bloque, mantiene el valor de cada iteración (0, 1, 2)
