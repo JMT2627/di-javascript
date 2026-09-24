09b9abc39917630fec234b56c10ba9fb0ffdb156
Jairo Montes Toyos <ish71606@educastur.es>
2026-09-24 18:30:58 +0200

    Nuevos ejemplos

---
+++ src/02-types/examples.js
@@ -0,0 +1,17 @@
+let nombre = "Juan";
+let saludo = "Hola";
+let mensaje = `Bienvenido ${nombre}`;
+
+let edad = 25;
+let pi = 3.1416;
+let infinito = Infinity;
+let noNumero = NaN; // sigue siendo tipo "number"
+
+let activo = true;
+let mayor = false;
+
+let x;
+console.log(x); // undefined
+
+let vacio = null;
+
